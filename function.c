477adc9919e7139eeb2f536d8fa3c8e018fbed0e
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-12-01 16:34:20 +0330

    add INT command for create integer variables

--- function.c
+++ function.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 void print_function(char *args, int number_line) {
   char *arg;
@@ -24,10 +25,21 @@ void shell_function(char *args, int number_line) {
   }
 }
 
+void int_function(char *args, int number_line) {
+  char *name;
+  int value;
+  strtok(args, " ");
+  name = strtok(NULL, " ");
+  value = atoi(strtok(NULL, " "));
+  printf("args: %s\nname: %s\nvalue: %d\n",args,name,value);
+}
+
 void execute_command(char *function, char *args, int number_line) {
   if (strcmp(function, "PRINT") == 0) {
     print_function(args, number_line);
   } else if (strcmp(function, "SHELL") == 0) {
     shell_function(args, number_line);
-  }
+  } else if (strcmp(function, "INT") == 0) {
+    int_function(args, number_line);
+  }  
 }
--- function.h
+++ function.h
@@ -3,9 +3,6 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
-void print_function (char* args, int number_line);
-void shell_function (char* args,int number_line);
-
 void execute_command(char* function, char* args, int number_line);
 
 #endif
