46c79581af96d546c2d7d12892644a173e51d6bd
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-12-04 18:39:32 +0000

    add error logger -> log.c log.h

---
+++ log.c
@@ -0,0 +1,8 @@
+#include "./log.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_error(char *msg, int number_line) {
+  printf("Line <%d> | %s\n",msg,number_line);
+  exit(1);
+}
---
+++ log.h
@@ -0,0 +1,6 @@
+#ifndef LOG_H
+#define LOG_H
+
+void print_error(cher *msg, int number_line);
+
+#endif
